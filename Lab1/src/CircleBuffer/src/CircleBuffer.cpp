e236b8c234b61ee0e8045f13f9d510b23478aa73
unknown <shabanov.dima11203@gmail.com>
2023-09-11 22:37:10 +0700

    Lab1

---
+++ Lab1/src/CircleBuffer/src/CircleBuffer.cpp
@@ -0,0 +1,277 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "../includes/CircleBuffer.hpp"
+
+namespace {
+template <typename T> T mod(const T &a, const T &b) { return (a % b + b) % b; }
+} // namespace
+
+CircularBuffer::CircularBuffer()
+    : m_capacity{0}, m_buffer{NULL}, m_start{-1}, m_end{-1} {}
+
+CircularBuffer::CircularBuffer(const CircularBuffer &cb)
+    : m_capacity{cb.m_capacity}, m_start{cb.m_start}, m_end{cb.m_end} {
+    m_buffer = new value_type[m_capacity];
+    for (int i = 0; i < m_capacity; i++) {
+        m_buffer[i] = cb.m_buffer[i];
+    }
+}
+
+CircularBuffer::CircularBuffer(int capacity)
+    : m_capacity{capacity}, m_start{0}, m_end{-1} {
+    m_buffer = new value_type[capacity];
+}
+
+CircularBuffer::CircularBuffer(int capacity, const value_type &elem)
+    : m_capacity{capacity}, m_start{0}, m_end{capacity - 1} {
+    m_buffer = new value_type[capacity];
+
+    for (size_t i = 0; i < m_capacity; i++) {
+        m_buffer[i] = elem;
+    }
+}
+
+CircularBuffer::~CircularBuffer() { delete[] m_buffer; }
+
+value_type &CircularBuffer::operator[](int i) {
+    return const_cast<value_type &>(
+        static_cast<const CircularBuffer *>(this)->operator[](i));
+}
+
+const value_type &CircularBuffer::operator[](int i) const {
+    int index = mod<int>(m_start + i, m_capacity);
+    return m_buffer[index];
+}
+
+value_type &CircularBuffer::at(int i) {
+    return const_cast<value_type &>(
+        static_cast<const CircularBuffer *>(this)->at(i));
+}
+
+const value_type &CircularBuffer::at(int i) const {
+    if (i > m_capacity)
+        throw std::out_of_range("Array index out of bounds");
+    return m_buffer[i];
+}
+
+value_type &CircularBuffer::front() {
+    return const_cast<value_type &>(
+        static_cast<const CircularBuffer *>(this)->front());
+} // Ссылка на первый элемент.
+
+const value_type &CircularBuffer::front() const { return m_buffer[m_start]; }
+
+value_type &CircularBuffer::back() {
+    return const_cast<value_type &>(
+        static_cast<const CircularBuffer *>(this)->back());
+} // Ссылка на последний элемент.
+
+const value_type &CircularBuffer::back() const { return m_buffer[m_end]; }
+
+value_type *CircularBuffer::linearize() {
+    value_type *tmp = new value_type[m_capacity];
+    for (int i = 0; i < m_capacity; i++) {
+        tmp[i] = this->at(i);
+    }
+    delete[] m_buffer;
+    m_buffer = tmp;
+    m_end = this->size() - 1;
+    m_start = 0;
+    return m_buffer;
+}
+
+bool CircularBuffer::is_linearized() const {
+    if (m_start == 0) {
+        return true;
+    }
+    return false;
+}
+
+void CircularBuffer::rotate(int new_begin) {
+    try {
+        if (!this->full()) {
+            throw new std::logic_error("not full");
+        }
+        int interval = new_begin;
+        int lastPos = 0;
+        int nextPos = interval + lastPos;
+        for (int i = 0; i < m_capacity; i++) {
+            value_type tmp = m_buffer[nextPos];
+            m_buffer[nextPos] = m_buffer[lastPos];
+            lastPos = nextPos;
+            nextPos = mod<int>(nextPos + interval, m_capacity);
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+int CircularBuffer::size() const {
+    if (m_end == -1) {
+        return 0;
+    }
+
+    int size = m_end >= m_start ? m_end - m_start + 1
+                                : m_capacity - (m_start - m_end) + 1;
+    return size;
+}
+
+bool CircularBuffer::empty() const {
+    if (this->size() == 0) {
+        return true;
+    }
+    return false;
+}
+
+bool CircularBuffer::full() const {
+    if (this->size() == this->capacity()) {
+        return true;
+    }
+    return false;
+}
+
+int CircularBuffer::reserve() const { return m_capacity - this->size(); }
+
+int CircularBuffer::capacity() const { return m_capacity; }
+
+void CircularBuffer::set_capacity(int new_capacity) {
+    this->linearize();
+    value_type *tmp = new value_type[new_capacity];
+
+    if (new_capacity < m_capacity) {
+        m_end = new_capacity - 1;
+        for (size_t i = 0; i < new_capacity; i++) {
+            tmp[i] = m_buffer[i];
+        }
+
+    } else {
+        for (size_t i = 0; i < m_capacity; i++) {
+            tmp[i] = m_buffer[i];
+        }
+    }
+    m_capacity = new_capacity;
+    delete[] m_buffer;
+    m_buffer = tmp;
+}
+
+void CircularBuffer::resize(int new_size, const value_type &item) {
+    this->linearize();
+    m_buffer = (value_type *)realloc(m_buffer, sizeof(value_type) * new_size);
+    if (new_size > m_capacity) {
+        for (size_t i = m_capacity - 1; i < new_size; i++) {
+            m_buffer[i] = item;
+        }
+    }
+    m_end = new_size - 1;
+
+    m_capacity = new_size;
+}
+
+CircularBuffer &CircularBuffer::operator=(const CircularBuffer &cb) {
+    if (m_buffer) {
+        delete[] m_buffer;
+    }
+
+    m_capacity = cb.m_capacity;
+    m_buffer = new value_type[m_capacity];
+
+    std::copy(cb.m_buffer, cb.m_buffer + m_capacity, m_buffer);
+
+    m_start = cb.m_start;
+    m_end = cb.m_end;
+
+    return *this;
+}
+
+void CircularBuffer::swap(CircularBuffer &cb) {
+    std::swap(m_buffer, cb.m_buffer);
+    std::swap(m_capacity, cb.m_capacity);
+    std::swap(m_start, cb.m_start);
+    std::swap(m_end, cb.m_end);
+}
+
+void CircularBuffer::push_back(const value_type &item) {
+
+    if (this->full()) {
+        m_end = mod<int>(m_end + 1, m_capacity);
+        m_start = mod<int>(m_start + 1, m_capacity);
+    } else {
+        m_end = mod<int>(m_end + 1, m_capacity);
+    }
+    m_buffer[m_end] = item;
+}
+
+void CircularBuffer::push_front(const value_type &item) {
+
+    if (this->full()) {
+        m_end = mod<int>(m_end - 1, m_capacity);
+        m_start = mod<int>(m_start - 1, m_capacity);
+
+    } else {
+        m_start = mod<int>(m_start - 1, m_capacity);
+    }
+    m_buffer[m_start] = item;
+}
+
+void CircularBuffer::pop_back() {
+    if (this->size() == 1) {
+        m_end = -1;
+        m_start = 0;
+    } else {
+        m_end = mod<int>(m_end - 1, m_capacity);
+    }
+}
+
+void CircularBuffer::pop_front() {
+    if (this->size() == 1) {
+        m_end = -1;
+        m_start = 0;
+    } else {
+        m_start = mod<int>(m_start + 1, m_capacity);
+    }
+}
+
+void CircularBuffer::insert(
+    int pos, const value_type &item) { // нужна доработка, чтобы обработать
+    if (pos > this->capacity())
+        throw std::logic_error("Cannot be inserted into such a position");
+    m_buffer[mod<int>(m_start + pos, m_capacity)] = item;
+}
+
+void CircularBuffer::erase(int first, int last) {
+    if (first != 0 && last != this->size() - 1)
+        throw std::logic_error("Can't delete the middle");
+    if (first == 0 && last == this->size() - 1) {
+        m_start = 0;
+        m_end = -1;
+    } else if (first == 0) {
+        m_start = mod<int>(m_start + (last - first), m_capacity);
+    } else if (last == this->size() - 1) {
+        m_end = mod<int>(m_end - (last - first), m_capacity);
+    }
+}
+
+void CircularBuffer::clear() {
+    m_end = -1;
+    m_start = 0;
+}
+
+bool operator==(const CircularBuffer &a, const CircularBuffer &b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    if (a.capacity() != b.capacity()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool operator!=(const CircularBuffer &a, const CircularBuffer &b) {
+    return !(a == b);
+}
---
+++ Lab1/tests/CircleBufferTests.cpp
@@ -0,0 +1,167 @@
+#include <CircleBuffer/includes/CircleBuffer.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(CircularBufferInitTest, InitTest) {
+    CircularBuffer cb1{};
+    CircularBuffer cb2{5};
+    CircularBuffer cb3{cb2};
+    CircularBuffer cb4{5, 'f'};
+
+    EXPECT_EQ(cb1.capacity(), 0);
+    EXPECT_TRUE(cb1.empty());
+
+    EXPECT_EQ(cb2.capacity(), 5);
+    EXPECT_TRUE(cb2.empty());
+
+    EXPECT_EQ(cb3.capacity(), 5);
+    EXPECT_EQ(cb2, cb3);
+
+    EXPECT_EQ(cb4.capacity(), 5);
+    EXPECT_EQ(cb4.size(), 5);
+}
+
+TEST(CircularBufferExtractTest, ExtractElementTest) {
+    CircularBuffer cb{5, 'b'};
+    const CircularBuffer cb1{5, 'b'};
+
+    cb.insert(0, 'a');
+    EXPECT_EQ(cb[0], 'a');
+    EXPECT_EQ(cb.at(0), 'a');
+
+    cb.push_back('t');
+    EXPECT_EQ(cb.back(), 't');
+
+    cb.push_front('r');
+    EXPECT_EQ(cb.front(), 'r');
+
+    EXPECT_EQ(cb1.back(), 'b');
+    EXPECT_EQ(cb1.front(), 'b');
+}
+
+TEST(CircularBufferMoveTest, MoveCellsTest) {
+    CircularBuffer cb{5, 'a'};
+    cb.rotate(3);
+    EXPECT_TRUE(cb.is_linearized());
+
+    cb.linearize();
+    EXPECT_TRUE(cb.is_linearized());
+}
+
+TEST(CircularBufferSizeTest, CheckSizeTest) {
+    CircularBuffer cb{5, 'a'};
+
+    EXPECT_FALSE(cb.empty());
+
+    EXPECT_EQ(cb.size(), 5);
+    EXPECT_EQ(cb.capacity(), 5);
+
+    EXPECT_TRUE(cb.full());
+
+    EXPECT_EQ(cb.reserve(), 0);
+}
+
+TEST(CircularBufferSizeTest, SetSizeTest) {
+    CircularBuffer cb{5, 'a'};
+
+    cb.set_capacity(6);
+    EXPECT_EQ(cb.capacity(), 6);
+    EXPECT_EQ(cb.size(), 5);
+
+    cb.resize(8, 'u');
+    EXPECT_EQ(cb.size(), 8);
+}
+
+TEST(CircularBufferSwapTest, SwapTest) {
+    CircularBuffer cb1{5};
+    CircularBuffer cb2{5, 'b'};
+    CircularBuffer tmp{cb1};
+    cb1 = cb2;
+    cb2 = tmp;
+    EXPECT_EQ(cb1.size(), 5);
+    EXPECT_EQ(cb2.size(), 0);
+
+    cb1.swap(cb2);
+    EXPECT_EQ(cb1.size(), 0);
+    EXPECT_EQ(cb2.size(), 5);
+}
+
+TEST(CircularBufferAddElementTest, AddElementTest) {
+    CircularBuffer cb1{5, 'c'};
+    CircularBuffer cb2{5};
+    cb1.push_back('a');
+    EXPECT_EQ(cb1.back(), 'a');
+    cb1.push_front('b');
+    EXPECT_EQ(cb1.front(), 'b');
+
+    cb2.push_back('a');
+    EXPECT_EQ(cb2.back(), 'a');
+
+    cb2.push_front('b');
+    EXPECT_EQ(cb2.front(), 'b');
+
+    cb1.insert(1, 'g');
+    EXPECT_EQ(cb1[1], 'g');
+
+    EXPECT_ANY_THROW(cb1.insert(10, '1'));
+}
+
+TEST(CircularBufferRemoveElementTest, RemoveElementTest) {
+    CircularBuffer cb1{5, 'a'};
+    CircularBuffer cb2{5};
+    CircularBuffer cb3{8, 'a'};
+    CircularBuffer cb4{7, 'f'};
+
+    cb1.pop_back();
+    EXPECT_EQ(cb1.size(), 4);
+
+    cb1.pop_front();
+    EXPECT_EQ(cb1.size(), 3);
+
+    cb2.push_back('a');
+    cb2.pop_back();
+    EXPECT_EQ(cb2.size(), 0);
+
+    cb2.push_back('a');
+    cb2.pop_front();
+    EXPECT_EQ(cb2.size(), 0);
+
+    cb1.clear();
+    EXPECT_EQ(cb1.size(), 0);
+
+    EXPECT_ANY_THROW(cb3.erase(1, 2));
+    cb3.erase(6, 7);
+    EXPECT_EQ(cb3.size(), 7);
+    cb3.erase(0, 1);
+    EXPECT_EQ(cb3.size(), 6);
+
+    cb4.erase(0, 6);
+    EXPECT_EQ(cb4.size(), 0);
+}
+
+TEST(CircularBufferEqTest, EqTest) {
+    CircularBuffer cb1{5};
+    CircularBuffer cb2{5};
+
+    EXPECT_TRUE(cb2 == cb1);
+
+    cb2.push_back('a');
+    EXPECT_FALSE(cb2 == cb1);
+
+    cb2.pop_back();
+    cb2.set_capacity(6);
+    EXPECT_FALSE(cb2 == cb1);
+
+    cb2.set_capacity(5);
+    cb1.push_back('a');
+    cb2.push_back('b');
+    EXPECT_FALSE(cb2 == cb1);
+
+    EXPECT_TRUE(cb1 != cb2);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
